f2f26a2e6ad4190d851b3560ce006c5dda87b85b
Marcos Renê <146996755+marcosreneGIT@users.noreply.github.com>
2023-11-20 19:33:22 -0300

    Create ex008.js

---
+++ aula011/ex008.js
@@ -0,0 +1,9 @@
+var vel = 60
+
+console.log(`A velocidade do carro estava em ${vel}km/h`)
+
+if (vel > 60){
+    console.log('Você ultrapassou o limete de velocidade. Multado!')
+}
+
+console.log('Dirija sempre com cinto de segurança!')
