9643799cd4c1969dbb989e440fd388a19e943c0f
Marcos Renê <146996755+marcosreneGIT@users.noreply.github.com>
2023-11-23 17:06:32 -0300

    aula13

---
+++ aula13/ex016.js
@@ -0,0 +1,11 @@
+var c = 0
+while (c < 5){
+    console.log(`Passo: ${c}`)
+    c ++
+}
+c = 0
+
+do{
+    console.log(`Passo: ${c}`)
+    c ++
+}while(c < 5)
---
+++ aula13/node_a45474b649eab.tmp.js
@@ -0,0 +1,10 @@
+var c = 0
+while (c < 5){
+    console.log(`Passo: ${c}`)
+    c ++
+}
+c = 0
+
+do{
+    console.log(`Passo: ${c}`)
+}while(c <= 5)
